a1d2c775eceabfac400f633529db45fc7acd4a55
taylor.yao <taylor.yao@carbit.com.cn>
2022-05-09 11:09:57 +0800

    [创建项目] First Commit

---
+++ app/src/main/java/com/ysp/mqcq02_applist/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ysp.mqcq02_applist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
